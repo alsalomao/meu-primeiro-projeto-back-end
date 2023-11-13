21d5aab3a5abd7857fda666c525101824e3650eb
alsalomao <alsalomao@outlook.com.br>
2023-11-13 16:37:26 -0300

    add a primeira versão do meu projeto

---
+++ mulher.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+function mostraMulher(request, response){
+
+        response.json(
+        {
+            nome: 'Mulher teste 1',
+            imagem: 'htttps://github.com/simaraconceicao.png',
+            minibio: 'Desenvolvedora e instrutura'
+        })
+}
+
+function mostraPorta() {
+    console.log('Servidor criado e rodando na porta ', porta)
+}
+
+app.use(router.get('/mulher', mostraMulher))
+app.listen(porta, mostraPorta)
